047e54abc0de3339c0cd9a9b19b45416f97678e2
Metto <mettovictor11@gmail.com>
2026-09-03 13:42:12 +0300

    feat: add fuctions to the file

---
+++ functions.js
@@ -0,0 +1,8 @@
+function addition(a, b) {
+    return a + b
+}
+console.log(addition(4, 63))
+
+fuction login(email, password) {
+    if (email == registeredEmail && password == registeredPassword) {
+        console.log("Login successful")
